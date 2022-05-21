e6813c3888da712000274917858fc3ce6402231f
MayconSantosBR <maycon.wb.santos@gmail.com>
2022-05-21 11:20:49 -0300

    first commit

---
+++ AtividadeInicial/AtividadeInicial/AtividadeInicial/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AtividadeInicial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var idade = 10;
+            int idadeInt = 12;
+            double meuDouble = 1500.00;
+            float meuFloat = 1500.00f;
+            decimal meuDecimal = 1500.00m;
+
+            bool verdadeiroFalso = true;
+
+            string nome = "C Sharp";
+            string[] alfabeto = { "A", "B" };
+
+            List<string> alfabetoLista = new List<string>() {"A", "B"};
+            Dictionary<int, string> chaveValor = new Dictionary<int, string>() { { 1, "A" }, { 2, "B" } };
+
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine(nome);
+
+            Console.WriteLine("-------------------------------------------");
+
+            foreach (string letra in alfabeto)
+            {
+                Console.WriteLine(letra);
+            }
+
+            Console.WriteLine("-------------------------------------------");
+
+            foreach (string letra in alfabetoLista)
+            {
+                Console.WriteLine(letra);
+            }
+
+            Console.WriteLine("-------------------------------------------");
+
+            foreach (var letra in chaveValor)
+            {
+                Console.WriteLine(letra);
+            }
+        }
+
+        public class Pessoa
+        {
+            public string nome { get; set; }
+        }
+    }
+}
